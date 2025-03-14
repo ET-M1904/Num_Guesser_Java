7e5afc33f493474b72afd7f5105724952932c324
ET-M1904 <erik.taylor@bsu.edu>
2025-03-14 14:51:50 +0000

    Progress

---
+++ numGuesser.java
@@ -0,0 +1,13 @@
+public class numGuesser {
+	public static void Main (String[] args) {
+		
+
+	}
+
+	public String menu() {
+		System.out.println("0) quit");
+		System.out.println("1) Human Guesser");
+		System.out.pritnln("2) Computer Guesser");
+		String response = input.nextLine();
+		return response;
+}
